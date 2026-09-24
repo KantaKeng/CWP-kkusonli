11339a33607bdea563f6e0b334fc1debf3fcdb64
Kantapon Kusonliem <kengptw@gmail.com>
2026-09-24 23:06:56 +0700

    LAST

---
+++ discovery_piscine/cell05/ex12/string_are_arrays.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+    else:
+        text = sys.argv[1]
+        count = text.count("z")
+        if count == 0:
+            print("none")
+        else:
+            print("z" * count)
+
+if __name__ == "__main__":
+    main()
